d0de4c7420df8a5536ce72f75b1f830ab792978c
glorrymiss <113041245+glorrymiss@users.noreply.github.com>
2023-12-12 07:56:11 +0100

    correct

--- src/components/Container/Container.styled.js
+++ src/components/Container/Container.styled.js
@@ -4,7 +4,6 @@ export const StyledContainer = styled.div`
   width: 100%;
   height: 100%;
   margin: 0 auto;
-  box-sizing: border-box;
 
   @media screen and (min-width: 768px) {
     width: 768px;
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,11 +1,14 @@
 import SectionAboutAniraK from 'components/SectionAboutAniraK/SectionAboutAniraK';
 import SectionHero from 'components/SectionHero/SectionHero';
+import { Container } from 'components/Container/Container';
+import { useTitle } from 'hooks';
 
 export const MainPage = () => {
+  useTitle('AniraK');
   return (
-    <>
+    <Container>
       <SectionHero />
       <SectionAboutAniraK />
-    </>
+    </Container>
   );
 };
