e0f26c902c31be9a5ef808ef240baf67a0a4453c
glorrymiss <113041245+glorrymiss@users.noreply.github.com>
2023-12-14 22:42:48 +0100

    Update BoxNavigation.js

--- src/components/BoxNavigation/BoxNavigation.js
+++ src/components/BoxNavigation/BoxNavigation.js
@@ -1,38 +1,60 @@
-import { useState } from 'react';
+import IconHeart from 'images/icons/IconHeart';
 import IconBag from '../../images/icons/IconBag';
-import IconHeart from '../../images/icons/IconHeart';
 import IconPerson from '../../images/icons/IconPerson';
-import { Box } from './BoxNavigation.styled';
+import { Box, Button } from './BoxNavigation.styled';
+import theme from 'components/theme';
+import { useAuth } from 'hooks';
+import { useState } from 'react';
+import ModalRegistration from 'components/Modal/ModalRegistration/ModalRegistration';
 import Modal from 'components/Modals/Modal';
-import ModalAuthorization from 'components/Modals/ModalAuthorization/ModalAuthorization';
 
-const BoxNavigation = () => {
-  const [isOpenModalAuthorization, setIsOpenModalAuthorization] =
-    useState(false);
+const BoxNavigation = ({ $mainPage }) => {
+  const [isModal, setIsModal] = useState(false);
+
+  const isOpenModal = () => {
+    setIsModal(true);
+  };
+
+  const isCloseModal = () => {
+    setIsModal(false);
+  };
+  const { currentTheme } = useAuth();
   return (
-    <>
-      <Box>
-        <IconHeart />
-        <button
-          onClick={() => {
-            setIsOpenModalAuthorization(true);
-          }}
-        >
-          <IconPerson />
-        </button>
-
-        <IconBag />
-      </Box>
-      {isOpenModalAuthorization && (
-        <Modal
-          onClick={() => {
-            setIsOpenModalAuthorization(false);
-          }}
-        >
-          <ModalAuthorization />
+    <Box>
+      <Button type="button">
+        <IconHeart
+          stroke={
+            $mainPage
+              ? theme[currentTheme].color.mainColor1
+              : theme[currentTheme].color.mainColor5
+          }
+        />
+      </Button>
+      <Button type="button" onClick={isOpenModal}>
+        <IconPerson
+          stroke={
+            $mainPage
+              ? theme[currentTheme].color.mainColor1
+              : theme[currentTheme].color.mainColor5
+          }
+        />
+      </Button>
+      {isModal && (
+        <Modal onClick={isCloseModal}>
+          <ModalRegistration />
         </Modal>
       )}
-    </>
+
+      <Button type="button">
+        <IconBag
+          stroke={
+            $mainPage
+              ? theme[currentTheme].color.mainColor1
+              : theme[currentTheme].color.mainColor5
+          }
+        />
+      </Button>
+    </Box>
   );
 };
 export default BoxNavigation;
