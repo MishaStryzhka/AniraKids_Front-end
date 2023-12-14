2612bfc06ab99d594439e294bd46ade26f2604e2
Mykhailo Stryzhka <mykhailo.stryzhka@seznam.cz>
2023-12-14 22:15:02 +0100

    Update BoxNavigation.js

--- src/components/BoxNavigation/BoxNavigation.js
+++ src/components/BoxNavigation/BoxNavigation.js
@@ -1,59 +1,14 @@
-import IconHeart from 'images/icons/IconHeart';
 import IconBag from '../../images/icons/IconBag';
+import IconHeart from '../../images/icons/IconHeart';
 import IconPerson from '../../images/icons/IconPerson';
-import { Box, Button } from './BoxNavigation.styled';
-import theme from 'components/theme';
-import { useAuth } from 'hooks';
-import { useState } from 'react';
-import Modal from 'components/Modal/Modal';
-import ModalRegistration from 'components/Modal/ModalRegistration/ModalRegistration';
+import { Box } from './BoxNavigation.styled';
 
 const BoxNavigation = ({ $mainPage }) => {
-  const [isModal, setIsModal] = useState(false);
-
-  const isOpenModal = () => {
-    setIsModal(true);
-  };
-
-  const isCloseModal = () => {
-    setIsModal(false);
-  };
-  const { currentTheme } = useAuth();
   return (
     <Box>
-      <Button type="button">
-        <IconHeart
-          stroke={
-            $mainPage
-              ? theme[currentTheme].color.mainColor1
-              : theme[currentTheme].color.mainColor5
-          }
-        />
-      </Button>
-      <Button type="button" onClick={isOpenModal}>
-        <IconPerson
-          stroke={
-            $mainPage
-              ? theme[currentTheme].color.mainColor1
-              : theme[currentTheme].color.mainColor5
-          }
-        />
-      </Button>
-      {isModal && (
-        <Modal onClick={isCloseModal}>
-          <ModalRegistration />
-        </Modal>
-      )}
-
-      <Button type="button">
-        <IconBag
-          stroke={
-            $mainPage
-              ? theme[currentTheme].color.mainColor1
-              : theme[currentTheme].color.mainColor5
-          }
-        />
-      </Button>
+      <IconHeart fill={'none'} stroke={$mainPage ? '#fff' : '#000'} />
+      <IconPerson />
+      <IconBag />
     </Box>
   );
 };
