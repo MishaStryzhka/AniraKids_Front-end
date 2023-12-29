d896660306bf9dfb93d329a3221280c739a5afdc
Mykhailo Stryzhka <mykhailo.stryzhka@seznam.cz>
2023-12-29 23:09:00 +0100

    adds register and login fetch

--- src/components/BoxNavigation/BoxNavigation.js
+++ src/components/BoxNavigation/BoxNavigation.js
@@ -4,15 +4,25 @@ import IconPerson from '../../images/icons/IconPerson';
 import { Box, Button } from './BoxNavigation.styled';
 import theme from 'components/theme';
 import { useAuth } from 'hooks';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ModalRegistration from 'components/Modals/ModalRegistration/ModalRegistration';
 import Modal from 'components/Modals/Modal';
+import { useNavigate } from 'react-router-dom';
 
 const BoxNavigation = ({ $mainPage }) => {
   const [isModal, setIsModal] = useState(false);
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  console.log('isModal', isModal);
+
+  useEffect(() => {
+    isModal && user && navigate('/my-account', { replace: true });
+    isModal && user && setIsModal(false);
+  }, [isModal, user, navigate]);
 
   const isOpenModal = () => {
-    setIsModal(true);
+    user ? navigate('/my-account', { replace: true }) : setIsModal(true);
   };
 
   const isCloseModal = () => {
@@ -39,12 +49,11 @@ const BoxNavigation = ({ $mainPage }) => {
           }
         />
       </Button>
-      {isModal && (
+      {isModal && !user && (
         <Modal onClick={isCloseModal}>
           <ModalRegistration />
         </Modal>
       )}
-
       <Button type="button">
         <IconBag
           stroke={
--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://project-be7v6c5s.onrender.com';
+axios.defaults.baseURL = 'https://anira-kids-back-end.onrender.com';
+// axios.defaults.baseURL = 'http://Localhost:4000';
 
 // Utility to add JWT
 const setAuthHeader = token => {
