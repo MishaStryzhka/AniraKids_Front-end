2d24b50c9dd0e16c27ab53d259ed106a4fd26112
MishaStryzhka <mykhailo.stryzhka@seznam.cz>
2024-02-27 18:08:33 +0000

    Deploying to gh-pages from @ MishaStryzhka/AniraKids_Front-end@efbcae3ada76e05dd586567ad142111aef85d06d 🚀

--- static/js/281.e3403c03.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkanira_kids=self.webpackChunkanira_kids||[]).push([[281],{6281:(a,s,e)=>{e.r(s),e.d(s,{default:()=>i});var r=e(184);const i=()=>(0,r.jsx)("p",{children:"Cart"})}}]);
-//# sourceMappingURL=281.e3403c03.chunk.js.map
