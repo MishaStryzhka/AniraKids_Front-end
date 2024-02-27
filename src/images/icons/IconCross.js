e35e52367e9dfc28aa616de0f5d41bfb321ac85c
Mykhailo Stryzhka <mykhailo.stryzhka@seznam.cz>
2024-02-27 19:05:19 +0100

    Update IconCross.js

--- src/images/icons/IconCross.js
+++ src/images/icons/IconCross.js
@@ -4,9 +4,7 @@ const IconCross = props => (
     xmlns="http://www.w3.org/2000/svg"
     width={24}
     height={24}
-    viewBox="1.5 2 20 20"
     fill="none"
-    stroke="#000"
     viewBox="0 0 24 24"
     {...props}
   >
