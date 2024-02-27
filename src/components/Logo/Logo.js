63e6e7f2b919eef943bc67a5b0e879fb6070f06f
glorrymiss <113041245+glorrymiss@users.noreply.github.com>
2024-02-27 10:04:53 +0100

    add correct size for logo for adaptive in header and footer

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,10 @@
 import { NavLink } from 'react-router-dom';
-import IconLogo from '../../images/icons/IconLogo';
-import IconLogoMin from 'images/icons/IconLogoMin';
+import { StyledIconLogo } from './Logo.styled';
 
 const Logo = () => {
-  const WidthScreen = window.innerWidth >= 1280;
   return (
     <NavLink to="./" style={{ display: 'flex' }}>
-      {WidthScreen ? <IconLogo /> : <IconLogoMin />}
+      <StyledIconLogo />
     </NavLink>
   );
 };
--- src/components/Logo/Logo.styled.js
+++ src/components/Logo/Logo.styled.js
@@ -2,6 +2,10 @@ import IconLogo from 'images/icons/IconLogo';
 import styled from 'styled-components';
 
 export const StyledIconLogo = styled(IconLogo)`
-  /* width: 40px;
-  height: 40px; */
+  width: 40px;
+  height: 40px;
+  @media screen and (min-width: 1280px) {
+    width: 80px;
+    height: 80px;
+  }
 `;
