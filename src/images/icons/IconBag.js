e3e30bd04720a3ca3d43f331229d8e91c57d434f
Misha.Strizhka <mykhailo.stryzhka@seznam.cz>
2024-02-29 02:12:47 +0100

    Update IconBag.js

--- src/images/icons/IconBag.js
+++ src/images/icons/IconBag.js
@@ -2,9 +2,9 @@ import * as React from 'react';
 const IconBag = ({ className, ...props }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
     className={className}
-    // width={className || 24}
-    // height={className || 24}
     viewBox="0 0 24 24"
     fill="none"
     // stroke="#000"
