fd25f96197eee907e0b270a560548e5c3b451056
MishaStryzhka <mykhailo.stryzhka@seznam.cz>
2024-03-13 22:06:06 +0000

    Deploying to gh-pages from @ MishaStryzhka/AniraKids_Front-end@dac969ef1122065e68acadd27b7f20c8bc7dad36 🚀

---
+++ static/js/700.98e72d2b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkanira_kids=self.webpackChunkanira_kids||[]).push([[700],{88700:(s,a,e)=>{e.r(a),e.d(a,{default:()=>n});var k=e(28152),i=e(80184);const n=()=>(0,i.jsx)(k.Z,{})}}]);
+//# sourceMappingURL=700.98e72d2b.chunk.js.map
