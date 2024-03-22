3ade0110e6a00cfa61797094ddea995de77ea9fa
Misha.Strizhka <mykhailo.stryzhka@seznam.cz>
2024-03-22 10:23:31 +0100

    Update FilterDate.js

--- src/components/Filters/FilterDate/FilterDate.js
+++ src/components/Filters/FilterDate/FilterDate.js
@@ -1,19 +1,28 @@
 import IconCalendar from 'images/icons/IconCalendar';
 import {
   ButtonCalendar,
+  ItemInfo,
+  ListInfo,
+  MarkerDelivery,
+  MarkerRent,
+  MarkerReturn,
   MenuButtonClose,
   MenuCalendar,
+  MenuDescription,
+  MenuHeader,
+  MenuTitle,
 } from './FilteDate.styled';
+import Calendar from 'components/Calendar/Calendar';
 import { useSearchParams } from 'react-router-dom';
 import IconClose from 'images/icons/IconClose';
-import CalendarSelectDate from 'components/Calendar/CalendarSelectDate';
-
+import ButtonAdd from 'components/Buttons/ButtonAdd/ButtonAdd';
 const { useState, useRef, useEffect } = require('react');
 
 const FilteDate = () => {
   const [isOpenModalCalendar, setIsOpenModalCalendar] = useState(false);
   const [searchParams, setSearchParams] = useSearchParams();
   const menuRef = useRef(null);
+
   const [rentalPeriods, setRentalPeriods] = useState(
     searchParams.get('rentalPeriods') || null
   );
@@ -40,7 +49,7 @@ const FilteDate = () => {
     };
   }, [isOpenModalCalendar]);
 
-  const saveSelectedDate = () => {
+  const setFilterDate = () => {
     newSetSearchParams('rentalPeriods', rentalPeriods);
     setIsOpenModalCalendar(false);
   };
@@ -61,18 +70,38 @@ const FilteDate = () => {
           ref={menuRef}
           style={{ position: 'absolute', zIndex: 999, backgroundColor: '#fff' }}
         >
-          <MenuButtonClose onClick={() => setIsOpenModalCalendar(false)}>
-            <IconClose width={24} height={24} />
-          </MenuButtonClose>
-          <CalendarSelectDate
+          <MenuHeader>
+            <MenuTitle>Календар оренди</MenuTitle>
+            <MenuButtonClose onClick={() => setIsOpenModalCalendar(false)}>
+              <IconClose width={24} height={24} />
+            </MenuButtonClose>
+          </MenuHeader>
+          <Calendar
             rentalPeriods={rentalPeriods}
-            setRentalPeriods={setRentalPeriods}
-            saveSelectedDate={() => saveSelectedDate()}
+            setRentalPeriods={rentalPeriods => setRentalPeriods(rentalPeriods)}
           />
+          <ListInfo>
+            <ItemInfo>
+              <MarkerDelivery /> <p>дні доставки</p> <br />
+            </ItemInfo>
+            <ItemInfo>
+              <MarkerRent /> <p>оренда</p> <br />
+            </ItemInfo>
+            <ItemInfo>
+              <MarkerReturn />
+              <p>день повернення</p> <br />
+            </ItemInfo>
+          </ListInfo>
+          <MenuDescription>
+            Виберіть день оренди <br />
+            (або дату початку та кінця оренди)
+          </MenuDescription>
+          <ButtonAdd disabled={!rentalPeriods} onClick={() => setFilterDate()}>
+            ЗБЕРЕГТИ ЗМІНИ
+          </ButtonAdd>
         </MenuCalendar>
       )}
     </>
   );
 };
-
 export default FilteDate;
