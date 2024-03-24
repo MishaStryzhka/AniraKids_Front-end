17a1e8bac385a1dc0f6f6c11ead5d384a05c7b55
Misha.Strizhka <mykhailo.stryzhka@seznam.cz>
2024-03-24 07:50:11 +0100

    Adds translation for SelectedFilters

--- src/components/Filters/SelectedFiltrs/translations/en.js
+++ src/components/Filters/SelectedFiltrs/translations/en.js
@@ -1,10 +1,120 @@
 export const SelectedFiltrsTranslationsEn = {
+  // FilterType
   type: 'Type',
   sale: 'Sale',
   rent: 'Rent',
 
   // Outfits
+  Outfits: 'Outfits',
   'for-girls': 'For girls',
   'for-boys': 'For boys',
   'for-babies': 'For babies',
+
+  //FilterToys
+  Toys: 'Toys',
+  'toys-for-baby': 'Toys for baby',
+  'dolls-and-pups': 'Dolls and pups',
+  'stuffed-toys': 'Stuffed toys',
+  constructors: 'Constructors',
+  'car-model': 'Cars, models, technology',
+  'for-recreation': 'For active recreation',
+  'board-games-and-puzzles': 'Board games, puzzles',
+  'toy-weapon': 'Toy weapon',
+  'creativity-and-development': 'Creativity and Development',
+  'school-supplies': 'School supplies',
+  'children`s-transport': 'Children`s transport',
+  'robots-and-transformers': 'Robots and transformers',
+
+  //FilterSubject
+  Subject: 'Subject',
+  Christmas: 'Christmas',
+  'Ukrainian-symbols': 'Ukrainian symbols',
+  Animals: 'Animals',
+  'ballet-&-princesses': 'Ballet and princesses',
+  Dinosaurs: 'Dinosaurs',
+  'Flowers-&-butterflies': 'Flowers and butterflies',
+  Hearts: 'Hearts',
+  'unicors-&-rainbows': 'Unicors and rainbows',
+
+  // FilterSort
+  'Sort by': 'Sort by',
+  popularity: 'popularity',
+  'expensive to cheap': 'from expensive to cheap',
+  'cheap to expensive': 'from cheap to expensive',
+  'new arrival': 'new arrival',
+
+  // FilterSizeChildren
+  Size: 'Size',
+  cm: 'cm',
+  'before-50': 'before 50',
+
+  // Price
+  Price: 'Price',
+
+  // FilterForPregnantWomen
+  'For pregnant women': 'For pregnant women',
+  true: 'Yes',
+  false: 'No',
+
+  // FilterFamilyLook
+  'for-all-family': 'For the whole family',
+  'for-mamy-and-girl': 'For mom and daughter',
+  'for-mamy-and-boy': 'For mom and son',
+  'for-dad-and-girl': 'For dad and girl',
+  'for-dad-and-boy': 'For dad and boy',
+
+  // FilterDecor
+  Decor: 'Decor',
+  accessories: 'Accessories',
+  birthday: 'Birthday',
+  'Theme-holidays': 'Theme holidays',
+  textile: 'Textile',
+  'blankets-for-baby': 'Blankets for baby',
+  linens: 'Bed Linens',
+
+  // FilterAge
+  Age: 'Age',
+  'for premature babies': 'for premature babies',
+  'for newborns': 'for newborns',
+  '0-3-months': '0-3 months',
+  '4-6-months': '4-6 months',
+  '7-9-months': '7-9 months',
+  '10-12-months': '10-12 months',
+  '13-18-months': '13-18 months',
+  '1.5-2-years': '1.5-2 years',
+  '2-3-years': '2-3 years',
+  '3-4-years': '3-4 years',
+  '4-5-years': '4-5 years',
+  '6-7-years': '6-7 years',
+  '8-9-years': '8-9 years',
+  '10-years': '10 years',
+  '11-years': '11 years',
+  '12-years': '12 years',
+  '13-years': '13 years',
+
+  // Color
+  colorful: 'colorful',
+  white: 'white',
+  gray: 'gray',
+  orange: 'orange',
+  turquoise: 'turquoise',
+  green: 'green',
+  black: 'black',
+  bordo: 'bordo',
+  lilac: 'lilac',
+  silvery: 'silvery',
+  yellow: 'yellow',
+  pink: 'pink',
+  blue: 'blue',
+  violet: 'violet',
+  lightGreen: 'light green',
+  coral: 'coral',
+  beige: 'beige',
+  golden: 'golden',
+  red: 'red',
+  khaki: 'khaki',
+  brown: 'brown',
+  lightBlue: 'light blue',
+  peachy: 'peachy',
+  mustard: 'mustard',
 };
--- src/components/Filters/SelectedFiltrs/translations/uk.js
+++ src/components/Filters/SelectedFiltrs/translations/uk.js
@@ -8,6 +8,88 @@ export const SelectedFiltrsTranslationsUk = {
   'for-boys': 'Для хлопчиків',
   'for-babies': 'Для малюків',
 
+  //FilterToys
+  Toys: 'Іграшки',
+  'toys-for-baby': 'Іграшки для малюків',
+  'dolls-and-pups': 'Ляльки та пупси',
+  'stuffed-toys': 'М’які іграшки',
+  constructors: 'Конструктори',
+  'car-model': 'Машинки, моделі, техніка',
+  'for-recreation': 'Для активного відпочинку',
+  'board-games-and-puzzles': 'Настільні ігри, пазли',
+  'toy-weapon': 'Іграшкова зброя',
+  'creativite-and-development': 'Творчість і розвиток',
+  'school-supplies': 'Товари для школи',
+  'children`s-transport': 'Дитячий транспорт',
+  'robots-and-transformers': 'Роботи і трансформери',
+
+  //FilterSubject
+  Subject: 'Тематика',
+  Christmas: 'Різдво',
+  'Ukrainian-symbols': 'Українська символіка',
+  Animals: 'Звірята',
+  'ballet-&-princesses': 'Балет та принцеси',
+  Dinosaurs: 'Динозаври',
+  'Flowers-&-butterflies': 'Квіти і метелики',
+  Hearts: 'Сердечки',
+  'unicors-&-rainbows': 'Єдинороги та веселки',
+
+  // FilterSort
+  'Sort by': 'Сортувати за',
+  popularity: 'популярністю',
+  'expensive to cheap': 'від дорогих до дешевих',
+  'cheap to expensive': 'від дешевих до дорогих',
+  'new arrival': 'новинка',
+
+  // FilterSizeChildren
+  Size: 'Розмір',
+  cm: 'см',
+  'before-50': 'до 50',
+
+  // Price
+  Price: 'Ціна',
+
+  // FilterForPregnantWomen
+  'For pregnant women': 'Для вагітних',
+  true: 'Так',
+  false: 'Ні',
+
+  // FilterFamilyLook
+  'for-all-family': 'Для всієї сім’ї',
+  'for-mamy-and-girl': 'Для мами і дівчинки',
+  'for-mamy-and-boy': 'Для мами і хлопчика',
+  'for-dad-and-girl': 'Для тата і дівчинки',
+  'for-dad-and-boy': 'Для тата і дівчинки',
+
+  // FilterDecor
+  Decor: 'Декор',
+  accessories: 'Аксесуари',
+  birthday: 'День народження',
+  'Theme-holidays': 'Тематичні свята',
+  textile: 'Текстиль',
+  'blankets-for-baby': 'Пледи для малюків',
+  linens: 'Постільна білизна',
+
+  // FilterAge
+  Age: 'Вік',
+  'for premature babies': 'для передчасно народжених дітей',
+  'for newborns': 'для новонароджених',
+  '0-3-months': '0-3 місяці',
+  '4-6-months': '4-6 місяців',
+  '7-9-months': '7-9 місяців',
+  '10-12-months': '10-12 місяців',
+  '13-18-months': '13-18 місяців',
+  '1.5-2-years': '1.5-2 роки',
+  '2-3-years': '2-3 роки',
+  '3-4-years': '3-4 роки',
+  '4-5-years': '4-5 років',
+  '6-7-years': '6-7 років',
+  '8-9-years': '8-9 років',
+  '10-years': '10 років',
+  '11-years': '11 років',
+  '12-years': '12 років',
+  '13-years': '13 років',
+
   // Color
   colorful: 'Різнокольоровий',
   white: 'Білий',
