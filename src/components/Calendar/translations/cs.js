82d4ffdbbfec7b999b6a4854a243d8175a8c3b0f
Misha.Strizhka <mykhailo.stryzhka@seznam.cz>
2024-04-08 20:32:03 +0200

    adds translation  CalendarSelectDate

--- src/components/Calendar/translations/cs.js
+++ src/components/Calendar/translations/cs.js
@@ -17,9 +17,12 @@ export const CalendarTranslationsCs = {
     menuTitle: 'Vyberte datum',
     daysDelivery: 'dny doručení',
     rental: 'pronájem',
+    up_to_6_hours: 'až 6 hodin',
     returnDay: 'den vrácení',
     menuDescription:
       'Vyberte den pronájmu (nebo datum začátku a konce pronájmu)',
     buttonSaveChanges: 'ULOŽIT ZMĚNY',
+    self_pickup: 'Vyzvednutí osobně',
+    return_within_6_hours: 'Vrátit do 6 hodin',
   },
 };
