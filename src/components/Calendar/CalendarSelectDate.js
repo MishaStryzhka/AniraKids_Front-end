3466249c58f53db8876870390a9393afdd3bd5e7
Misha.Strizhka <mykhailo.stryzhka@seznam.cz>
2024-04-18 10:14:07 +0200

    create CalendarDescription

--- src/components/Calendar/CalendarSelectDate.js
+++ src/components/Calendar/CalendarSelectDate.js
@@ -1,17 +1,13 @@
 import ButtonAdd from 'components/Buttons/ButtonAdd/ButtonAdd';
 import Calendar from './Calendar';
 import {
-  ItemInfo,
-  ListInfo,
-  MarkerDelivery,
-  MarkerRent,
-  MarkerReturn,
   MenuDescription,
   MenuTitle,
   WrapCalendar,
 } from './CalendarSelectDate.styled';
 import { useTranslation } from 'react-i18next';
 import SelectTypeRent from 'components/SelectTypeRent/SelectTypeRent';
+import CalendarDescription from './components/CalendarDescription/CalendarDescription';
 
 const CalendarSelectDate = ({
   rentalPeriods,
@@ -40,43 +36,8 @@ const CalendarSelectDate = ({
         rentalPeriods={rentalPeriods}
         setRentalPeriods={rentalPeriods => setRentalPeriods(rentalPeriods)}
       />
-      <ListInfo>
-        <ItemInfo>
-          {typeRent === 'celebration' ? (
-            <>
-              <MarkerDelivery /> <p>{t('daysDelivery')}</p>
-            </>
-          ) : (
-            <>
-              <MarkerRent />
-              <p>{t('self_pickup')}</p>
-            </>
-          )}
-          <br />
-        </ItemInfo>
-        <ItemInfo>
-          <MarkerRent />{' '}
-          <p>
-            {t('rental')}
-            {typeRent !== 'celebration' && ` ${t('up_to_6_hours')}`}
-          </p>{' '}
-          <br />
-        </ItemInfo>
-        <ItemInfo>
-          {typeRent === 'celebration' ? (
-            <>
-              <MarkerReturn />
-              <p>{t('returnDay')}</p>
-            </>
-          ) : (
-            <>
-              <MarkerRent />
-              <p>!!!{t('return_within_6_hours')}!!!</p>
-            </>
-          )}
-          <br />
-        </ItemInfo>
-      </ListInfo>
+      <CalendarDescription typeRent={typeRent} />
+
       <MenuDescription>{t('menuDescription')}</MenuDescription>
       <ButtonAdd disabled={!rentalPeriods} onClick={() => saveSelectedDate()}>
         {t('buttonSaveChanges')}
--- src/components/Calendar/CalendarSelectDate.styled.js
+++ src/components/Calendar/CalendarSelectDate.styled.js
@@ -31,35 +31,6 @@ export const MenuButtonClose = styled.button`
   justify-content: center;
 `;
 
-export const ListInfo = styled.ul`
-  display: flex;
-  flex-direction: column;
-  gap: 6px;
-`;
-export const ItemInfo = styled.li`
-  display: flex;
-  gap: 6px;
-`;
-
-export const MarkerDelivery = styled.div`
-  width: 20px;
-  height: 20px;
-  background-color: rgb(170, 230, 123);
-  border-radius: 20px;
-`;
-export const MarkerRent = styled.div`
-  width: 20px;
-  height: 20px;
-  background-color: #ebdad1;
-  border-radius: 20px;
-`;
-export const MarkerReturn = styled.div`
-  width: 20px;
-  height: 20px;
-  background-color: red;
-  border-radius: 20px;
-`;
-
 export const MenuDescription = styled.p`
   text-align: center;
 `;
