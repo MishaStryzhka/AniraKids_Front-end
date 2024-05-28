c1c964c8b834f5a31c72ad2ea73fa3722e3e2529
Misha.Strizhka <mykhailo.stryzhka@seznam.cz>
2024-05-28 11:29:22 +0200

    Adds Caunt

--- src/components/BoxNavigation/BoxNavigation.styled.js
+++ src/components/BoxNavigation/BoxNavigation.styled.js
@@ -13,4 +13,28 @@ export const Button = styled.button`
   border: none;
   cursor: pointer;
   padding: 0;
+
+  position: relative;
+`;
+
+export const Caunt = styled.p`
+  position: absolute;
+  top: -3px;
+  right: -3px;
+
+  width: 14px;
+  height: 14px;
+  border-radius: 50%;
+
+  background-color: ${({ theme }) => theme.color.additionalColorBrown};
+
+  font-weight: 700;
+  font-size: 8px;
+  line-height: 150%;
+  letter-spacing: 0em;
+  text-align: center;
+  color: ${({ theme }) => theme.color.mainColor5};
+
+  text-align: center;
+  align-content: center;
 `;
