f3f758ec71fa06b0b283e754b8f86cfef490b999
MishaStryzhka <mykhailo.stryzhka@seznam.cz>
2026-09-20 22:29:30 +0000

    Deploying to gh-pages from @ MishaStryzhka/AniraKids_Front-end@7ae41739dcdfeefcc6965d1fa418be775a689822 🚀

---
+++ static/js/463.7b570ebc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkanira_kids=self.webpackChunkanira_kids||[]).push([[463],{87463:(e,n,s)=>{s.r(n),s.d(n,{default:()=>c});var r=s(72791),a=s(94420),t=s(11087),i=s(82565),o=s(80184);const c=()=>{const[e,n]=(0,t.lr)(),s=e.get("token"),c=(0,a.I0)();return(0,r.useEffect)((()=>{c((0,i.confirmUserEmail)({token:s}))}),[c,s]),console.log("token",s),(0,o.jsx)("p",{children:"ConfirmEmailPage"})}}}]);
+//# sourceMappingURL=463.7b570ebc.chunk.js.map
